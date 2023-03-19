b41fd3f17ea3bcfc2a488144ef70b9460722ee7a
PG727_ <114147739+CylamYeeting@users.noreply.github.com>
2023-03-19 15:17:08 +0800

    Create a011.cpp

---
+++ a011.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int a, b;
+	string run;
+	while(cin>>a>>run>>b){
+	if(run=="*"){
+		cout<<a*b<<endl;
+	}else if(run=="-"){
+		cout<<a-b<<endl;
+	}else if(run=="/"){
+		cout<<a/b<<endl;
+	}else{
+		cout<<a+b<<endl;
+	}
+	}
+}
