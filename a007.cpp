5883e6b02c6517b0828fc9caac65e1d6d8cfdd0f
PG727_ <114147739+CylamYeeting@users.noreply.github.com>
2023-03-19 15:25:25 +0800

    Create a007.cpp

---
+++ a007.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	string girl; getline(cin, girl);
+	string boy; getline(cin, boy);
+	cout<<girl<<" and "<<boy<<" sitting in the tree";
+}
