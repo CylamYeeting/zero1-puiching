c4569d7b97ea14890484675edd527ac53406c071
PG727_ <114147739+CylamYeeting@users.noreply.github.com>
2023-03-19 17:16:11 +0800

    Create a023.cpp

---
+++ a023.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int year; cin>>year;
+	if(year%4==0){
+		if(year%100==0){
+			if(year%400==0){
+				cout<<"a leap year";
+			}else{
+				cout<<"a normal year";
+			}
+		}else{
+			cout<<"a leap year";	
+		}
+	}else{
+		cout<<"a normal year";
+	}
+}
