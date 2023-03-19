dd2b92c2f62740cf4f86e73816771f37460e1ef8
PG727_ <114147739+CylamYeeting@users.noreply.github.com>
2023-03-19 15:05:05 +0800

    Create a004.cpp

---
+++ a004.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int n; cin>>n;
+	int cost; 
+	int ans=0;
+	for(int i=1;i<=n;i++){
+		cin>>cost;
+		ans = ans + cost * i;
+	}
+	cout<<ans;
+}
