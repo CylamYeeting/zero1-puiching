f7b60779a6d4c74618f9b8c8932cacc85de52b2b
PG727_ <114147739+CylamYeeting@users.noreply.github.com>
2023-03-25 11:06:16 +0800

    Update a666.cpp

--- a666.cpp
+++ a666.cpp
@@ -1,19 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	//basic cin
-	int v, n; cin>>v, n;
-	int a[n+1];
-	for(int i=1;i<=n;i++) cin>>a[i];
-	
-	// turn everything to zero
-	int dpm[v+1];	
-	//main code
-	for(int i=1;i<=n;i++){
-		for(int j=v;j>=a[i];j--){
-			dpm[j] = max(dpm[j], dpm[j-a[i]]+a[i]);//me take if no take just stand still cuz is going backward, not going to effect if u not going to take the item		
+	int v, n; cin>>v>>n;
+	int A[n];
+	int answer=0;
+	for(int i=1;i<=n;i++) cin>>A[i];
+	int nm[n+1][v+1];
+	for(int i=0;i<=n;i++){
+		for(int j=0;j<=v;j++){
+			nm[i][j]=0;
+		}
 	}
-}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=v;j++){
+			if(j-A[i]>=0){
+				nm[i][j] = max(nm[i-1][j], nm[i-1][j-A[i]]+A[i]);			
+			}else{
+				nm[i][j]=nm[i-1][j];
+			}
+		}
+	}
 
-	cout<<v-dpm[v]<<endl;	 //cout answer;
+	cout<<v-nm[n][v];	
 }
