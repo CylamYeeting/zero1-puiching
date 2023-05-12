4cd657970c253c596557dab244faebee80cc0c56
PG727_ <114147739+CylamYeeting@users.noreply.github.com>
2023-05-12 17:35:42 +0800

    Create a031.cpp

---
+++ a031.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i; cin>>i;
+	if(i>=6 && i<=11){
+		cout<<"590";
+	}else if(i>=12 && i<=17){
+		cout<<"790";
+	}else if(i>=18 && i<=59){
+		cout<<"890";
+	}else if (i>=60){
+		cout<<"399";
+	}else{
+		cout<<"0";
+	}
+}
